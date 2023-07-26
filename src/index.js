cabc1a1651c1787bf517d40ccf2bebe14fe4fb37
Bellusfennec <bellusfennec@gmail.com>
2023-07-26 09:32:51 +0300

    refactor: :tada: init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import * as actions from "./store/actions";
+import { initiateStore } from "./store/store";
+// import { compose, pipe } from "lodash/fp";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = initiateStore();
+
+const App = () => {
+  const [state, setState] = useState(store.getState());
+
+  const completeTask = (id) => {
+    store.dispatch(actions.taskCompleted(id));
+  };
+  const changeTitle = (id) => {
+    store.dispatch(actions.titleChanges(id));
+  };
+
+  useEffect(() => {
+    store.subscribe(() => setState(store.getState()));
+  }, []);
+
+  return (
+    <>
+      <h1>App</h1>
+
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p> {`Compelete: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Complete</button>
+            <button onClick={() => changeTitle(el.id)}>Change Title</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
